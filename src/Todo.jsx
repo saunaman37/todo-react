b273de2d824d47a0f20f119531bcaca4f018d327
saunaman37 <ptiger1015@gmail.com>
2025-05-24 11:15:50 +0900

    HTMLでマークアップ

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,5 +1,40 @@
 import './style.css';
 
 export const Todo = () => {
-  return <div>hoge</div>;
+  return(
+    <>
+    <div>
+      <input placeholder='TODOを入力' />
+      <button>追加</button>
+    </div>
+    <div>
+      <p>未完了のTODO</p>
+      <ul>
+        <li>
+          <p>TODOです</p>
+          <button>完了</button>
+          <button>削除</button>
+        </li>
+        <li>
+          <p>TODOです</p>
+          <button>完了</button>
+          <button>削除</button>
+        </li>
+      </ul>
+    </div>
+    <div>
+    <p>完了のTODO</p>
+      <ul>
+        <li>
+          <p>TODOですた</p>
+          <button>戻す</button>
+        </li>
+        <li>
+          <p>TODOですた</p>
+          <button>戻す</button>
+        </li>
+      </ul>
+    </div>
+    </>
+  );
 };
